37b2a36d024da9025405b59822c613d6824fafe1
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-21 15:28:06 +0900

    typing骨組み

---
+++ typing.js
@@ -0,0 +1,17 @@
+// 必要なHTML要素の取得
+let wrap = document.getElementById('wrap');
+let start = document.getElementById('start');
+
+let textLists = []; // 複数のテキストを格納する配列
+
+let createText = () => {}; // ランダムなテキストを画面に表示する
+
+let keyDown = e => {}; // キーイベント&入力判定処理
+
+let rankCheck = rank => {}; // ランク判定とメッセージ生成処理
+
+let gameOver = id => {}; // ゲームの終了処理
+
+let timer = () => {}; // タイマー処理
+
+start.addEventListener('click', () => {}); // ゲームスタート時の処理
