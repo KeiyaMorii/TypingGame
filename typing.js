12b20f495405dcb0b7694a6faab816694b2fe9fb
KeiyaMorii <kei8.m.0123@gmail.com>
2021-01-24 14:39:43 +0900

    5章完了

--- typing.js
+++ typing.js
@@ -37,25 +37,54 @@ checkTexts = textLists[rnd].split('').map(value => {
 }; // ランダムなテキストを画面に表示する
 createText();
 
+// スコアの初期値を設定する
+let score = 0;
+
 let keyDown = e => {
     if(e.key === checkTexts[0].textContent) {
         // add-colorクラスを付与する
-        checkTexts[0].className = 'add-color'
-
+        checkTexts[0].className = 'add-color';
         // 配列から1文字を削除する
         checkTexts.shift();
+        // 正しい入力の時だけスコアを加算する
+        score++;
 
         // 最後まで入力したら新しいテキストを用意する
         if(!checkTexts.length) createText();
+        // Shiftキーを押した時は色が変わらない
+    } else if(e.key === 'Shift') {
+        wrap.style.backgroundColor = '#666';
+        // タイプミスした時だけ背景色を赤色に変える
+    } else {
+        wrap.style.backgroundColor = 'red';
     }
 }; // キーイベント&入力判定処理
 
-let rankCheck = rank => {}; // ランク判定とメッセージ生成処理
+let rankCheck = score => {
+    // テキストを格納する変数を作る
+    let text = '';
+    // スコアに応じて異なるメッセージを変数textに格納する
+    if(score < 100) {
+        text = `あなたのランクはCです。\nBランクまであと${100 - score}文字です。`;
+    } else if(score < 200) {
+        text = `あなたのランクはBです。\nAランクまであと${200 - score}文字です。`;
+    } else if(score < 300) {
+        text = `あなたのランクはAです。\nSランクまであと${300 - score}文字です。`;
+    } else if(score >= 300) {
+        text = `あなたのランクはSです。\nおめでとうございます！`;
+    }
+    // 生成したメッセージと一緒に文字列を返す
+    return `${score}文字打てました！\n${text}\n【OK】リトライ／【キャンセル】終了`;
+}; // ランク判定とメッセージ生成処理
 
 let gameOver = id => {
     // タイマーをストップする
     clearInterval(id);
-    console.log('ゲーム終了！')
+    
+    // スコアの値をrankCheck()に渡してダイアログで結果を表示する
+    let result = confirm(rankCheck(score));
+    // OKボタンをクリックされたらリロードする
+    if(result) window.location.reload();
 }; // ゲームの終了処理
 
 let timer = () => {
